cc644ff4760918bbb04aef1374efdbaf41d5fd3e
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-04 13:52:27 -0500

    fixed typo

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,23 @@ import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useEffect, useState } from "react";
 
 const contractAddress = "0x156cCEe877f2568FD895bDAaa6E31CBE236a08aD";
+const address = "0x593c44Df9E15906E329103c740CEBdbfA956180b";
+
 export default function Home() {
-  const [nfts, steNfts] = useState([]);
+  const [nfts, setNfts] = useState([]);
 
   useEffect(() => {
     let initSDK = async () => {
       const sdk = new ThirdwebSDK("rinkeby");
       const nftModule = sdk.getNFTModule(contractAddress);
-      const nftListWithOwnerAddress = nftModule.getAllWithOwner();
-      console.log(nftListWithOwnerAddress);
-      steNfts(nftListWithOwnerAddress);
+      const nftListWithOwnerAddress = await nftModule.getAllWithOwner();
+
+      setNfts(nftListWithOwnerAddress);
     };
     initSDK();
   }, []);
+
+  console.log(nfts);
 
   return (
     <div className="p-5">
@@ -27,6 +31,7 @@ export default function Home() {
         Welcome Everyone To My NFT Collection!
       </h1>
 
+      
       {nfts.map((nft) => {
         <div className="" key={nft.metadata.id}>
           <p>{nft.metadata.name}</p>
@@ -35,6 +40,7 @@ export default function Home() {
           <p>Owned by: {nft.owner}</p>
         </div>;
       })}
+      
     </div>
   );
 }
